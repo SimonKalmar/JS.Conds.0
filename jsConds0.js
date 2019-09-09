0d304e8f080ca7ea2a2ad69c93689da88e77db6d
Simon Kalmar Riis Mortensen <simonkalmar@gmail.com>
2019-09-09 22:05:07 +0200

    first commit

---
+++ jsConds0.js
@@ -0,0 +1,203 @@
+'use strict';
+
+let one = 0;
+let two = 0;
+let three = 0;
+let four = 0;
+let five = 0;
+let six = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 6 + 1);
+if (flip === 1)
+    one++;
+else if (flip === 2)
+    two++;
+
+    else if (flip === 3)
+        three++;
+
+        else if (flip === 4)
+            four++;
+
+            else if (flip === 5)
+                five++;
+
+                else
+                    six++;
+
+                    flip = Math.random();
+                    flip = Math.floor(flip * 6 + 1);
+                    if (flip === 1)
+                        one++;
+                    else if (flip === 2)
+                        two++;
+
+                        else if (flip === 3)
+                            three++;
+
+                            else if (flip === 4)
+                                four++;
+
+                                else if (flip === 5)
+                                    five++;
+
+                                    else
+                                        six++;
+
+
+                                        flip = Math.random();
+                                        flip = Math.floor(flip * 6 + 1);
+                                        if (flip === 1)
+                                            one++;
+                                        else if (flip === 2)
+                                            two++;
+
+                                            else if (flip === 3)
+                                                three++;
+
+                                                else if (flip === 4)
+                                                    four++;
+
+                                                    else if (flip === 5)
+                                                        five++;
+
+                                                        else
+                                                            six++;
+                                                            flip = Math.random();
+                                                            flip = Math.floor(flip * 6 + 1);
+                                                            if (flip === 1)
+                                                                one++;
+                                                            else if (flip === 2)
+                                                                two++;
+
+                                                                else if (flip === 3)
+                                                                    three++;
+
+                                                                    else if (flip === 4)
+                                                                        four++;
+
+                                                                        else if (flip === 5)
+                                                                            five++;
+
+                                                                            else
+                                                                                six++;
+                                                                                flip = Math.random();
+                                                                                flip = Math.floor(flip * 6 + 1);
+                                                                                if (flip === 1)
+                                                                                    one++;
+                                                                                else if (flip === 2)
+                                                                                    two++;
+
+                                                                                    else if (flip === 3)
+                                                                                        three++;
+
+                                                                                        else if (flip === 4)
+                                                                                            four++;
+
+                                                                                            else if (flip === 5)
+                                                                                                five++;
+
+                                                                                                else
+                                                                                                    six++;
+                                                                                                    flip = Math.random();
+                                                                                                    flip = Math.floor(flip * 6 + 1);
+                                                                                                    if (flip === 1)
+                                                                                                        one++;
+                                                                                                    else if (flip === 2)
+                                                                                                        two++;
+
+                                                                                                        else if (flip === 3)
+                                                                                                            three++;
+
+                                                                                                            else if (flip === 4)
+                                                                                                                four++;
+
+                                                                                                                else if (flip === 5)
+                                                                                                                    five++;
+
+                                                                                                                    else
+                                                                                                                        six++;
+
+                                                                                                                        flip = Math.random();
+                                                                                                                        flip = Math.floor(flip * 6 + 1);
+                                                                                                                        if (flip === 1)
+                                                                                                                            one++;
+                                                                                                                        else if (flip === 2)
+                                                                                                                            two++;
+
+                                                                                                                            else if (flip === 3)
+                                                                                                                                three++;
+
+                                                                                                                                else if (flip === 4)
+                                                                                                                                    four++;
+
+                                                                                                                                    else if (flip === 5)
+                                                                                                                                        five++;
+
+                                                                                                                                        else
+                                                                                                                                            six++;
+
+                                                                                                                                            flip = Math.random();
+                                                                                                                                            flip = Math.floor(flip * 6 + 1);
+                                                                                                                                            if (flip === 1)
+                                                                                                                                                one++;
+                                                                                                                                            else if (flip === 2)
+                                                                                                                                                two++;
+
+                                                                                                                                                else if (flip === 3)
+                                                                                                                                                    three++;
+
+                                                                                                                                                    else if (flip === 4)
+                                                                                                                                                        four++;
+
+                                                                                                                                                        else if (flip === 5)
+                                                                                                                                                            five++;
+
+                                                                                                                                                            else
+                                                                                                                                                                six++;
+                                                                                                                                                                flip = Math.random();
+                                                                                                                                                                flip = Math.floor(flip * 6 + 1);
+                                                                                                                                                                if (flip === 1)
+                                                                                                                                                                    one++;
+                                                                                                                                                                else if (flip === 2)
+                                                                                                                                                                    two++;
+
+                                                                                                                                                                    else if (flip === 3)
+                                                                                                                                                                        three++;
+
+                                                                                                                                                                        else if (flip === 4)
+                                                                                                                                                                            four++;
+
+                                                                                                                                                                            else if (flip === 5)
+                                                                                                                                                                                five++;
+
+                                                                                                                                                                                else
+                                                                                                                                                                                    six++;
+
+
+                                                                                                                                                                                    flip = Math.random();
+                                                                                                                                                                                    flip = Math.floor(flip * 6 + 1);
+                                                                                                                                                                                    if (flip === 1)
+                                                                                                                                                                                        one++;
+                                                                                                                                                                                    else if (flip === 2)
+                                                                                                                                                                                        two++;
+
+                                                                                                                                                                                        else if (flip === 3)
+                                                                                                                                                                                            three++;
+
+                                                                                                                                                                                            else if (flip === 4)
+                                                                                                                                                                                                four++;
+
+                                                                                                                                                                                                else if (flip === 5)
+                                                                                                                                                                                                    five++;
+
+                                                                                                                                                                                                    else
+                                                                                                                                                                                                        six++;
+console.log('ones: ' + one);
+console.log('twos: ' + two);
+console.log('threes: ' + three);
+console.log('fours: ' + four);
+console.log('fives: ' + five);
+console.log('sixes: ' + six);
